5b11ce18adf44ce4b0e08118a017d95e9cb72ce0
Artem Mikulich <artemmikulich@coherentsolutions.com>
2022-06-13 16:49:03 +0300

    Lab 1

---
+++ BadAdvisor/BadAdvisor.Mvc/Controllers/MessagesController.cs
@@ -0,0 +1,48 @@
+﻿using BadAdvisor.Mvc.Data;
+using BadAdvisor.Mvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadAdvisor.Mvc.Controllers
+{
+    [Route("messages")]
+    public class MessagesController : Controller
+    {
+        private static readonly Random Rand = new (DateTime.UtcNow.Millisecond);
+        private readonly IMessagesRepository _messagesRepository;
+        private readonly ISanitizerService _sanitizerService;
+
+        public MessagesController(IMessagesRepository messagesRepository, ISanitizerService sanitizerService)
+        {
+            _messagesRepository = messagesRepository;
+            _sanitizerService = sanitizerService;
+        }
+
+        [HttpGet("random")]
+        public async Task<IActionResult> GetRandom()
+        {
+            var maxNumber = _messagesRepository.GetTotalCount() - 1;
+
+            var message = await _messagesRepository.Get(Rand.Next(maxNumber));
+
+            if (IsBadWordsFilterEnabled())
+            {
+                message.Text = _sanitizerService.SanitizeBadWords(message.Text);
+            }
+
+            return new JsonResult(new MessageModel()
+            {
+                Text = message.Text ,
+            });
+        }
+
+        private bool IsBadWordsFilterEnabled()
+        {
+            return false;
+        }
+    }
+
+    public class MessageModel
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ BadAdvisor/BadAdvisor.Mvc/Services/SanitizerService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Text;
+
+namespace BadAdvisor.Mvc.Services
+{
+    public interface ISanitizerService
+    {
+        string SanitizeBadWords(string phrase);
+    }
+
+    public class SanitizerService : ISanitizerService
+    {
+        private static readonly string[] StopWords = { "fuck", "shit" };
+
+        public string SanitizeBadWords(string phrase)
+        {
+            foreach (var stopWord in StopWords)
+            {
+                phrase = phrase.Replace(stopWord, "***", StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            return phrase;
+        }
+    }
+}
